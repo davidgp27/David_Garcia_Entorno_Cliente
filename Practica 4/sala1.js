78f127b2d71a50b6feccdf0098c4ea4ac056cb5b
davidgp27 <davidgarciap27@gmail.com>
2025-12-09 00:09:51 +0100

    Objeto
    Se crea el objeto, y el Symbol

---
+++ Practica 4/sala1.js
@@ -0,0 +1,13 @@
+const tesoroSymbol = Symbol("tesoro");
+
+const cofre = {
+  estado: "cerrado",
+  contenido: {
+    mensaje: "El cofre está dañado. Necesitas clonar este objeto."
+  },
+
+  abrir() {
+    return this[tesoroSymbol];
+  },
+  [tesoroSymbol]: " Un diamante brillante"
+};
