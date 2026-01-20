d43763fe5de28ef3dd38ddb0ea968bbf7fb074f7
davidgp27 <davidgarciap27@gmail.com>
2026-01-20 20:21:09 +0100

    encapsulado de datos y lógica con propiedades privadas
    Se encapsulan las preguntas y contadores usando propiedades y métodos privados.

--- Practica 4/salas3/SalaAdivinanzas.js
+++ Practica 4/salas3/SalaAdivinanzas.js
@@ -2,30 +2,37 @@ import Sala from "./sala.js";
 
 export default class SalaAdivinanzas extends Sala {
 
+  #preguntas;
+  #indice;
+  #aciertos;
+
   constructor() {
     super("Sala de Adivinanzas");
 
-    this.preguntas = [
+    this.#preguntas = [
       { pregunta: "Oro parece, plata no es", respuesta: "platano" },
       { pregunta: "Blanco por dentro, verde por fuera", respuesta: "pera" },
       { pregunta: "Tiene agujas y no cose", respuesta: "reloj" }
     ];
 
-    this.indice = 0;
-    this.aciertos = 0;
+    this.#indice = 0;
+    this.#aciertos = 0;
   }
 
   obtenerPregunta() {
-    return this.preguntas[this.indice].pregunta;
+    return this.#preguntas[this.#indice].pregunta;
   }
 
   responder(texto) {
-    if (texto.toLowerCase() === this.preguntas[this.indice].respuesta) {
-      this.aciertos++;
-      this.indice++;
+    if (this.#comprobarRespuesta(texto.toLowerCase())) {
+      this.#aciertos++;
+      this.#indice++;
       return "Correcto";
     }
+  }
 
-    return "Incorrecto";
+  #comprobarRespuesta(respuesta) {
+    return respuesta === this.#preguntas[this.#indice].respuesta;
   }
 }
+
