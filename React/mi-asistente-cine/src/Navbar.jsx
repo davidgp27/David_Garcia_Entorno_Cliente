293af8580c570556dcfa910241d2c415aa75b884
davidgp27 <davidgarciap27@gmail.com>
2026-03-08 22:27:33 +0100

    Navegador
    se añade la barra del navegador y sus funciones, además el css de la aplicación principal.

---
+++ React/mi-asistente-cine/src/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+export default function Navbar({ darkMode, setDarkMode }) {
+  const [menuAbierto, setMenuAbierto] = useState(false); // useState obligatorio 
+
+  return (
+    <nav className="navbar">
+      <div className="nav-links">
+        <Link to="/">Inicio</Link>
+        <Link to="/chat">Asistente</Link>
+      </div>
+      <button onClick={() => setDarkMode(!darkMode)} className="theme-btn">
+        {darkMode ? '☀️ Modo Claro' : '🌙 Modo Oscuro'}
+      </button>
+    </nav>
+  );
+}
